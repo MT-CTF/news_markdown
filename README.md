20bdc927e9f6e0487b29200deca1324ddf6ef0d6
LoneWolfHT <lonewolf04361@gmail.com>
2022-10-26 13:03:31 -0700

    Custom CTF Changes

--- README.md
+++ README.md
@@ -7,3 +7,14 @@ All news is stored in markdown files in the world directory. They should be name
 On the news dialogue is a checkbox asking the player if they want to see the news when they join next time. This can be toggled on and off through the formspec, or by doing `/toggle_news`. Players can also run `/news` to quickly access the server news.
 
 If there are news updates (`news_markdown` will check the English news each time someone logs in for updates), players will either see the news pop up like normal or be told that there are updates in case they wish to see them.
+
+
+## Custom CTF Changes
+* Fixed crashes when news files aren't found
+* Moved news files to <worldir>/news/ to keep things clean
+* Removed the formspec showing on login to prevent collision with CTF formspecs
+* Added a HUD message in addition to chat message notifying of new updates, made both of them ~green
+* Redid the hash calculations to save to player meta, not a single modstorage key
+* Changed formspec to take files from memory instead of reading the file every time.
+* Added async function to load translation files
+* Added command to reload translation files, allowing for live updates
--- init.lua
+++ init.lua
@@ -1,128 +1,163 @@
-local storage = minetest.get_mod_storage()
-local prefix = "newsOnJoinExceptions_"
+local S = minetest.get_translator("news_markdown")
 
 local colors = {
-    background_color = "#FFF0",
-    font_color = "#FFF",
-    heading_1_color = "#AFF",
-    heading_2_color = "#FAA",
-    heading_3_color = "#AAF",
-    heading_4_color = "#FFA",
-    heading_5_color = "#AFF",
-    heading_6_color = "#FAF",
-    heading_1_size = "26",
-    heading_2_size = "24",
-    heading_3_size = "22",
-    heading_4_size = "20",
-    heading_5_size = "18",
-    heading_6_size = "16",
-    code_block_mono_color = "#6F6",
-    code_block_font_size = 14,
-    mono_color = "#6F6",
-    block_quote_color = "#FFA",
+	background_color = "#FFF0",
+	font_color = "#FFF",
+	heading_1_color = "#AFF",
+	heading_2_color = "#FAA",
+	heading_3_color = "#AAF",
+	heading_4_color = "#FFA",
+	heading_5_color = "#AFF",
+	heading_6_color = "#FAF",
+	heading_1_size = "26",
+	heading_2_size = "24",
+	heading_3_size = "22",
+	heading_4_size = "20",
+	heading_5_size = "18",
+	heading_6_size = "16",
+	code_block_mono_color = "#6F6",
+	code_block_font_size = 14,
+	mono_color = "#6F6",
+	block_quote_color = "#FFA",
 }
 
--- Replace the "newshash" key of mod storage with the SHA1 of the English news file
-local function update_hash()
-    local english_news = io.open(minetest.get_worldpath() .. "/news_en.md", "r")
+local news_files = {}
+local current_hash
+local loaded_files = false
 
-    storage:set_string("newshash", minetest.sha1(english_news:read("*a")))
-    english_news:close()
+-- Check the player's last seen news hash against the current hash, if they are different then notify of updates
+local function check_hash(player)
+	if not loaded_files then
+		minetest.after(1, check_hash, player)
+		return
+	elseif not news_files["news_en.md"] then
+		minetest.log("warning", "[news_markdown] News is either set up incorrectly, or isn't set up at all")
+		return
+	end
 
-    minetest.log("action", "Updated news hash in mod storage")
+	local meta = player:get_meta()
+
+	if current_hash ~= meta:get_string("news_markdown:last_seen_hash") then
+		minetest.chat_send_player(player:get_player_name(), minetest.colorize("green", "There are news updates, type /news to see them"))
+
+		hud_events.new(player, {
+			text = "There are news updates, type /news to see them",
+			color = "success",
+		})
+	end
 end
 
--- Check the stored news hash against the current hash, if they are different then there are updates
-local function check_for_updates(name)
-    local english_news = io.open(minetest.get_worldpath() .. "/news_en.md", "r")
-    local english_news_data = english_news:read("*a")
+-- Loads the news files into memory
+local function update_news_files(name)
+	loaded_files = false
 
-    if (storage:get_string("newshash") ~= minetest.sha1(english_news_data)) then
-        update_hash()
+	minetest.handle_async(function()
+		local news_dir = minetest.get_worldpath() .. "/news/"
+		local dirs = minetest.get_dir_list(news_dir, false)
+		local news_files_update = {}
 
-        if (storage:get_int(prefix .. name) == 1) then
-            minetest.chat_send_player(name, "There are news updates, type /news to see them")
-        end
+		for _, filename in pairs(dirs) do
+			if filename:match("news_.+%.md") then
+				local file, err = io.open(news_dir .. filename, "r")
 
-        minetest.log("action", "News updates found")
-    end
+				if not file then
+					minetest.log("error", err)
+				else
+					news_files_update[filename] = file:read("*a")
+					file:close()
+				end
+			end
+		end
 
-    english_news:close()
+		return news_files_update
+	end, function(news_files_update)
+		news_files = news_files_update
+
+		if news_files["news_en.md"] then
+			current_hash = minetest.sha1(news_files["news_en.md"])
+			loaded_files = true
+		end
+
+		for _, p in ipairs(minetest.get_connected_players()) do
+			check_hash(p)
+		end
+
+		if name then
+			minetest.chat_send_player(name, "News files updated.")
+		end
+	end)
 end
 
-local function show_news_formspec(name)
-    local player_info = minetest.get_player_information(name)
-    local language_code = player_info.lang_code
+local function show_news_formspec(name, lang_code_forced)
+	local language_code = minetest.get_player_information(name).lang_code
 
-    if (language_code == "") then
-        language_code = "en"
-    end
+	if language_code == "" then
+		language_code = "en"
+	end
 
-    local news_formspec = "formspec_version[5]" ..
-        "size[25, 15]" ..
-        "noprepend[]" ..
-        "bgcolor[" .. colors.background_color .. "]" ..
-        "button_exit[21.8, 13.8; 3, 1;exit; OK]" ..
-        "checkbox[0.2, 14.4;dont_show_again; Don't show me this again; false]"
+	if lang_code_forced and lang_code_forced ~= "" then
+		language_code = lang_code_forced
+	end
 
-    local news_filename = minetest.get_worldpath() .. "/news_" .. language_code .. ".md"
-    local news_file = io.open(news_filename, "r")
-    local news_markdown = news_file:read("*a")
-    news_file:close()
+	local news_formspec = "formspec_version[5]" ..
+		"size[25,15]" ..
+		"noprepend[]" ..
+		"bgcolor[" .. colors.background_color .. "]" ..
+		"button_exit[11,14;3,0.9;exit;OK]" ..
+		(language_code == "en" and
+			"button[0.1,14;5,0.9;switch_back;" or
+			"button[0.1,14;5,0.9;switch_en;"
+		) ..
+		S("Toggle English Translation") .."]"
 
-    news_formspec = news_formspec .. md2f.md2f(0.2, 0.2, 24.8, 13.4, news_markdown, "server_news", colors)
 
-    -- Gotta log 'em all!
-    minetest.log("action", "Showing news to " .. name .. " in language " .. language_code)
-    minetest.show_formspec(name, "server_news", news_formspec)
-    
-    minetest.register_on_player_receive_fields(function(player, formname, fields)
-        name = player:get_player_name()
+	local news = news_files["news_" .. language_code .. ".md"]
 
-        -- Don't do anything when the exit button is clicked, because no checkbox data is sent then
-        if not fields.exit then
-            if (fields.dont_show_again == "true") then
-                storage:set_int(prefix .. name, 1)
-            else
-                storage:set_int(prefix .. name, 0)
-            end
+	if not news then
+		if language_code ~= "en" then
+			show_news_formspec(name, "en")
+			return
+		end
 
-            minetest.log("action", "Toggled newsOnJoinExceptions_" .. name .. " to " .. tostring(storage:get_int(prefix .. name)))
-        end
-    end)
+		return
+	end
+
+	minetest.get_player_by_name(name):get_meta():set_string("news_markdown:last_seen_hash", current_hash)
+
+	news_formspec = news_formspec .. md2f.md2f(0.2, 0.2, 24.8, 13.4, news, "hypertext", colors)
+
+	-- Gotta log 'em all!
+	minetest.log("action", "Showing news to " .. name .. " in language " .. language_code)
+	minetest.show_formspec(name, "news_markdown:server_news", news_formspec)
 end
+
+minetest.register_on_player_receive_fields(function(player, formname, fields)
+	if formname ~= "news_markdown:server_news" then return end
+
+	local name = player:get_player_name()
+
+	if fields.switch_en then
+		show_news_formspec(name, "en")
+	elseif fields.switch_back then
+		show_news_formspec(name)
+	end
+end)
 
 minetest.register_on_joinplayer(function(player)
-    local name = player:get_player_name()
-    
-    -- Only show news to players who want to see it
-    if (storage:get_int(prefix .. name) == 0) then
-        show_news_formspec(name)
-    end
-
-    check_for_updates(name)
+	check_hash(player)
 end)
 
 minetest.register_chatcommand("news", {
-    description = "Shows the server news",
-    func = show_news_formspec
+	description = "Shows the server news",
+	func = show_news_formspec
 })
 
-minetest.register_chatcommand("toggle_news", {
-    description = "Toggles showing the news to you when you log in",
-    func = function(name)
-        local current_state = storage:get_int(prefix .. name)
-
-        if (current_state == 0) then
-            storage:set_int(prefix .. name, 1)
-            minetest.chat_send_player(name, "You will no longer see automatic news")
-
-            minetest.log("action", name .. " disabled automatic news")
-        else
-            storage:set_int(prefix .. name, 0)
-            minetest.chat_send_player(name, "You will now see automatic news")
-
-            minetest.log("action", name .. " enabled automatic news")
-        end
-    end
+minetest.register_chatcommand("update_news", {
+	description = "Checks for news updates",
+	privs = {server = true},
+	func = update_news_files,
 })
+
+minetest.after(0, function()
+	update_news_files()
+end)
